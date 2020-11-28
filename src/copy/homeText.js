9159dace18fe7ac8c72c606dd58dfbcdf1d34102
Andrew Mason <andrewmasonmedia@gmail.com>
2020-11-28 16:16:16 -0500

    Added a homeText file with an object containing all home page text for easy editing

---
+++ src/copy/homeText.js
@@ -0,0 +1,38 @@
+export const text = { 
+    section_1: {
+        "title": "What We Do.",
+        "subtitle": "At OGS we provide fun, engaging, goal-oriented music lessons for students of all ages.",
+        "desc": "Oakwood Guitar School is a well equipped home studio that provides a comfortable, relaxed learning environment for beginner to advanced guitar students, both children and adults of any age. Based in Toronto we service the areas of Regal Heights, Humewood, Hillcrest Village, Wychwood, Cedarvale, Forest Hill and St. Clair West. We also offer lessons over Zoom, Facetime, Skype or in home." 
+    },
+    section_2: {
+        "title": "Why OGS?",
+        "subtitle": "Some reasons why Oakwood Guitar School is a great place to learn.",
+        col1: {
+            "title": "Experienced Teachers" ,
+            "desc": "Our teachers are university-educated, active professional musicians with years of experience playing and teaching music.",
+        }, 
+        col2: {
+            "title": "All Ages & Levels" ,
+            "desc": "We teach students of all ages, abilities, and experience levels. Whether beginner or advanced, we can help you achieve your goals.",
+        }, 
+        col3: {
+            "title": "Perform and Record" ,
+            "desc": "At the end of each semester we offer recordings and recitals as an opportunity for our students to show off their new skills to friends & family!",
+        }, 
+        col4: {
+            "title": "Certified Programs" ,
+            "desc": "We teach certified programs from the Registry of Guitar Tutors and Conservatory Canada upon request in addition to essential skills and repertoire.",
+        }, 
+    },
+    section_3: {
+        "title": "What You Need.",
+        "subtitle": "New to guitar?  Here are a few things you'll need for lessons at Oakwood Guitar School",
+        "desc": "An acoustic guitar is recommended for new students, however electric guitar will also work just fine if that is what you have or what you're more interested in.  If the student is under 11 years old a smaller scale guitar is recommended, probably a 3/4 or half size guitar." 
+    },
+    section_4: {
+        "title": "What happens in a lesson?",
+        "subtitle": "Here's how a typical lesson works at Oakwood Guitar School",
+        "desc": "Lessons at OGS are personalized and tailored to the needs of each individual student. However they do follow a relatively consistent routine which includes: warm ups and stretches, technical exercises, ear training, rhythm practice, repertoire development, and other essential skills." 
+    },
+
+}//end copy object
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
-import React from "react"
-import { graphql } from "gatsby"
+import React from "react";
+import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import { Row, Col, Button } from "react-bootstrap";
-import Img from "gatsby-image"
+import * as Constants from '../copy/homeText';
+import Img from "gatsby-image";
 import AniLink from "gatsby-plugin-transition-link/AniLink";
 import MyForm from "../components/ContactForm";
 import scrollTo from 'gatsby-plugin-smoothscroll';
 
 export default function Home({data}) {
-
+  const text = Constants.text;
   return (
     <div>
       <Layout>
@@ -78,10 +79,13 @@ export default function Home({data}) {
             <Row>
               <Col lg={6} className="align-left-center bg-white rounded shadow-lg p-5 m-4">
                 <h1 className="section-title mb-5">
-                  What We Do.
+                  {text.section_1.title}
                 </h1>
-                <h2 className="h4 text-muted font-weight-bold">At OGS we provide fun, engaging, goal-oriented music lessons for students of all ages.</h2>
-                <p className="my-3">Oakwood Guitar School is a well equipped home studio that provides a comfortable, relaxed learning environment for beginner to advanced guitar students, both children and adults of any age. Based in Toronto we service the areas of Regal Heights, Humewood, Hillcrest Village, Wychwood, Cedarvale, Forest Hill and St. Clair West. We also offer lessons over Zoom, Facetime, Skype or in home.</p>
+                <h2 className="h4 text-muted font-weight-bold">{text.section_1.subtitle}
+                </h2>
+                <p className="my-3">
+                {text.section_1.desc}
+                </p>
                 <AniLink cover to="/why-ogs">
                   <Button variant="dark" className="mt-4 shadow">Learn more</Button>
                 </AniLink>
@@ -94,9 +98,11 @@ export default function Home({data}) {
         <Row className="min-100 p-0 m-0 text-light bg-black relative">
           <Col sm={12} className="align-left-top p-5">
             <h1 className="section-title pt-5 pb-3">
-              Why OGS?
+              {text.section_2.title}
             </h1>
-            <h2 className="h5 font-weight-bold">Some reasons why Oakwood Guitar School is a great place to learn</h2>
+            <h2 className="h5 font-weight-bold">
+              {text.section_2.subtitle}
+            </h2>
           </Col>
           <Col lg={3} md={6} className="align-left-top p-5">
             <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-users" width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#14CFA0" fill="none" strokeLinecap="round" strokeLinejoin="round">
@@ -106,8 +112,10 @@ export default function Home({data}) {
               <path d="M16 3.13a4 4 0 0 1 0 7.75" />
               <path d="M21 21v-2a4 4 0 0 0 -3 -3.85" />
             </svg>
-            <h5 className="py-3 border-bottom border-secondary mb-3 w-100">Experienced Teachers</h5>
-            <p>Our teachers are university-educated, active professional musicians with years of experience playing and teaching music.</p>
+            <h5 className="py-3 border-bottom border-secondary mb-3 w-100">
+              {text.section_2.col1.title}
+            </h5>
+            <p>{text.section_2.col1.desc}</p>
             <AniLink cover to="/why-ogs">
               <Button variant="outline-light" className="mt-4 shadow">Learn more</Button>
             </AniLink>
@@ -123,8 +131,8 @@ export default function Home({data}) {
               <line x1="11" y1="12" x2="20" y2="12" />
               <line x1="11" y1="18" x2="20" y2="18" />
             </svg>
-            <h5 className="py-3 border-bottom border-secondary mb-3 w-100">All Ages & Levels</h5>
-            <p>We teach students of all ages, abilities, and experience levels. Whether beginner or advanced, we can help you achieve your goals.</p>
+            <h5 className="py-3 border-bottom border-secondary mb-3 w-100">{text.section_2.col2.title}</h5>
+            <p>{text.section_2.col2.desc}</p>
             <AniLink cover to="/why-ogs">
               <Button variant="outline-light" className="mt-4 shadow">Learn more</Button>
             </AniLink>
@@ -136,8 +144,8 @@ export default function Home({data}) {
               <path d="M4 5v14l12 -7z" />
               <line x1="20" y1="5" x2="20" y2="19" />
             </svg>
-            <h5 className="py-3 border-bottom border-secondary mb-3 w-100">Perform and Record</h5>
-            <p>At the end of each semester we offer recordings and recitals as an opportunity for our students to show off their new skills to friends & family!</p>
+            <h5 className="py-3 border-bottom border-secondary mb-3 w-100">{text.section_2.col3.title}</h5>
+            <p>{text.section_2.col3.desc}</p>
             <AniLink cover to="/why-ogs">
               <Button variant="outline-light" className="mt-4 shadow">Learn more</Button>
             </AniLink>
@@ -153,8 +161,8 @@ export default function Home({data}) {
                 <line x1="6" y1="12" x2="9" y2="12" />
                 <line x1="6" y1="15" x2="8" y2="15" />
               </svg>
-            <h5 className="py-3 border-bottom border-secondary mb-3 w-100">Certified Programs</h5>
-            <p>We teach certified programs from the Registry of Guitar Tutors and Conservatory Canada upon request in addition to essential skills and repertoire.</p>
+            <h5 className="py-3 border-bottom border-secondary mb-3 w-100">{text.section_2.col4.title}</h5>
+            <p>{text.section_2.col4.desc}</p>
             <AniLink cover to="/why-ogs">
               <Button variant="outline-light" className="mt-4 shadow">Learn more</Button>
             </AniLink>
@@ -173,10 +181,10 @@ export default function Home({data}) {
             <Row>
               <Col lg={6} className="align-left-center bg-white shadow-lg rounded p-5 m-4">
                 <h1 className="section-title mb-5">
-                  What You Need.
+                  {text.section_3.title}
                 </h1>
-                <h2 className="h4 text-muted font-weight-bold">New to guitar?  Here are a few things you'll need for lessons at Oakwood Guitar School</h2>
-                <p className="my-3">An acoustic guitar is recommended for new students, however electric guitar will also work just fine if that is what you have or what you're more interested in.  If the student is under 11 years old a smaller scale guitar is recommended, probably a 3/4 or half size guitar.</p>
+                <h2 className="h4 text-muted font-weight-bold">{text.section_3.subtitle}</h2>
+                <p className="my-3">{text.section_3.desc}</p>
                 <AniLink cover to="/what-you-need">
                   <Button variant="dark" className="my-3">Learn more</Button>
                 </AniLink>
@@ -195,10 +203,10 @@ export default function Home({data}) {
           <Col lg={{span: 8, offset: 4}} className="align-left-center p-4 min-50">
             <div className="bg-ogs-green text-light shadow-lg rounded p-5">
               <h1 className="section-title mb-5">
-                What happens in a lesson?
+                {text.section_4.title}
               </h1>
-              <h2 className="h4 font-weight-bold">Here's how a typical lesson works at Oakwood Guitar School</h2>
-              <p className="my-3">Lessons at OGS are personalized and tailored to the needs of each individual student. However they do follow a relatively consistent routine which includes: warm ups and stretches, technical exercises, ear training, rhythm practice, repertoire development, and other essential skills.</p>
+              <h2 className="h4 font-weight-bold">{text.section_4.subtitle}</h2>
+              <p className="my-3">{text.section_4.desc}</p>
               <AniLink cover to="/typical-lesson">
                 <Button variant="light" className="my-3">Learn more</Button>
               </AniLink>
